d166c3180f30eb3fc1e37a331c9ee2aea2f166ef
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-08 16:38:11 -0600

    Creating the connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,11 @@
 // Set up mysql connection
 let mysql = require("mysql");
 
+// Defining connection
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+})
