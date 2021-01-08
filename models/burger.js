17d6c0265e9b4074f3981336046a71ff5fa146fb
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-08 17:40:00 -0600

    creating the burger object and functions

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,27 @@
 // Import ORM to create function that will interact with database
+const { createBrotliCompress } = require("zlib");
 let orm = require("../config/orm.js");
+
+// Creating the function to export
+let burger = {
+    all: function(cb) {
+        orm.all("burger", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb) {
+        orm.create("burger", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    update: function(ojbColVals, condition, cb) {
+        orm.update("burger", ojbColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    delete: function(condition, cb) {
+        orm.delete("burger", condition, function(res) {
+            cb(res);
+        });
+    }
+};
