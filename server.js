fd1e44af483e4f7c211d2be545b40f1c16ea9dd6
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-08 18:00:29 -0600

    Adding comments and defining express app

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+// Dependencies
 let express = require("express");
 let exphbs = require("express-handlebars");
 let routes = require("./controllers/burgers_controller.js");
 
+// Defining port
 const PORT = process.env.PORT || 8080;
+
+// Defining express app
+let app = express();
