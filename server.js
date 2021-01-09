84611e737b95bbb4f7f9cdd83946e660737cee47
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-08 18:01:49 -0600

    Defining static content

--- server.js
+++ server.js
@@ -8,3 +8,6 @@ const PORT = process.env.PORT || 8080;
 
 // Defining express app
 let app = express();
+
+// Serve static content for the app from the "public" directory
+app.use(express.static("public"));
