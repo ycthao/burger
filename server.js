de177166bcf931e34cec22fa410477f3e65e0232
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-08 18:02:25 -0600

    Parse application body as JSON

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ let app = express();
 
 // Serve static content for the app from the "public" directory
 app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
