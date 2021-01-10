da7b3cd29ef61848b18ff7a596e1043e08c1d722
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-10 17:47:13 -0600

    minor change in formating

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@ let orm = require("../config/orm.js");
 
 // Creating the function to export
 let bjs = {
-    selectAll: function(cb) {
-        orm.selectAll("burger", function(res) {
+    selectAll: function (cb) {
+        orm.selectAll("burger", function (res) {
             cb(res);
         });
     },
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burger", cols, vals, function(res) {
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burger", cols, vals, function (res) {
             cb(res);
         });
     },
-    updateOne: function(ojbColVals, condition, cb) {
-        orm.updateOne("burger", ojbColVals, condition, function(res) {
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burger", objColVals, condition, function (res) {
             cb(res);
         });
     }
