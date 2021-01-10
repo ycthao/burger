2baa9a830ebdecc1533c4a2476ae014aee7765fd
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-10 00:52:20 -0600

    creating route to get all

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,12 @@ let router = express.Router();
 
 let burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        let hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
