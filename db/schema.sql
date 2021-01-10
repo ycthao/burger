a45017e221dfb2b58e56e7c81298db18a2cc5449
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-10 12:00:12 -0600

    Creating burger table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS burgers_db;
 -- Creates the "todolist" database --
 CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burger (
+	id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (ID)
+);
