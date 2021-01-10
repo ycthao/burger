5cf20d40701ef51960a505d50ed36eef68a48a3f
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-09 22:01:12 -0600

    Adding printQuestionMarks function

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,12 @@
 // Importing MYSQL connection
 let connection = require("../config/connection.js");
 
+function printQuesetionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
