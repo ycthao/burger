77a1391685f3a118e0ab2dc74e16cef2947944d4
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-01-10 12:35:41 -0600

    update function to match assignment given name

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,21 @@
 // Import ORM to create function that will interact with database
-const { createBrotliCompress } = require("zlib");
+//const { createBrotliCompress } = require("zlib");
 let orm = require("../config/orm.js");
 
 // Creating the function to export
 let burger = {
-    all: function(cb) {
+    selectAll: function(cb) {
         orm.all("burger", function(res) {
             cb(res);
         });
     },
-    create: function(cols, vals, cb) {
+    insertOne: function(cols, vals, cb) {
         orm.create("burger", cols, vals, function(res) {
             cb(res);
         });
     },
-    update: function(ojbColVals, condition, cb) {
+    updateOne: function(ojbColVals, condition, cb) {
         orm.update("burger", ojbColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-    delete: function(condition, cb) {
-        orm.delete("burger", condition, function(res) {
             cb(res);
         });
     }
