cb7d06c8b16820df9fd308706c0e995270c7c5f5
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 12:51:31 -0600

    refining connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,34 @@
 // Set up mysql connection
 let mysql = require("mysql");
-let connection; 
+let connection;
 
 // Defining connection
 connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db",
 });
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "hacktheplanet",
-        database: "todoagain_db"
-    });
-};
-
+  connection = mysql.createConnection({
+    host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "s4po7jfle084djun",
+    password: "m4evw9rnk9mt8s9i",
+    database: "svmnb316hf6s5w4c",
+  });
+}
 
 // Making the connection
-connection.connect(function(err) {
-    if (err) {
-        console.error("Error connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as id " + connection.threadId);
 });
 
 // Export connection
