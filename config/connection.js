af22e0a9565deb7b4bd324b55efbf03cf75b6545
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 17:21:38 -0600

    taking out comment

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,6 @@
 let mysql = require("mysql");
 let connection;
 
-// Defining connection
-// connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "burgers_db",
-// });
-
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
