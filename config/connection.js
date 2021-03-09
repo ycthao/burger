de897622068812b38a3dbe83da69e3d2a2674905
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-09 12:54:14 -0600

    commenting out original connection to test for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,23 @@ let mysql = require("mysql");
 let connection;
 
 // Defining connection
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db",
-});
+// connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db",
+// });
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "klbcedmmqp7w17ik.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "s4po7jfle084djun",
-    password: "m4evw9rnk9mt8s9i",
-    database: "svmnb316hf6s5w4c",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
   });
 }
 
